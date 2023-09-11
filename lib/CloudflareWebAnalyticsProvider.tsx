99daf1dccff0a6dc9db60e255aa7664191305f83
Jamieson Roberts <hello@jamiesonroberts.com>
2023-09-10 22:57:07 -0400

    A start

--- lib/CloudflareWebAnalyticsProvider.tsx
+++ lib/CloudflareWebAnalyticsProvider.tsx
@@ -1,1 +1,10 @@
-export default function CloudflareWebAnalyticsProvider() {}
+import React, { ReactNode } from 'react'
+import Script from 'next/script'
+
+export default function CloudflareWebAnalyticsProvider(props: {
+    enabled?: boolean
+    token: string
+    children?: ReactNode | ReactNode[]
+}) {
+    return <></>
+}
