75cea1b94875a207e46d905d3c2c57deee23b464
Jamieson Roberts <hello@jamiesonroberts.com>
2023-09-11 08:42:35 -0400

    Working on baseline

--- lib/CloudflareWebAnalyticsProvider.tsx
+++ lib/CloudflareWebAnalyticsProvider.tsx
@@ -1,10 +1,35 @@
 import React, { ReactNode } from 'react'
 import Script from 'next/script'
 
+const cloudflareBeaconUrl: string =
+    'https://static.cloudflareinsights.com/beacon.min.js'
+
 export default function CloudflareWebAnalyticsProvider(props: {
-    enabled?: boolean
     token: string
+    spaDisable?: boolean
+    enabled?: boolean
     children?: ReactNode | ReactNode[]
+    scriptProps?: React.DetailedHTMLProps<
+        React.ScriptHTMLAttributes<HTMLScriptElement>,
+        HTMLScriptElement
+    >
 }) {
-    return <></>
+    const {
+        enabled = process.env.NODE_ENV === 'production' &&
+            (!process.env.NEXT_PUBLIC_VERCEL_ENV ||
+                process.env.NEXT_PUBLIC_VERCEL_ENV === 'production'),
+    } = props
+
+    return (
+        <>
+            {enabled && (
+                <Script
+                    src={`${cloudflareBeaconUrl}?${[
+                        `token=${props.token}`,
+                        props.spaDisable ? 'spa=false' : null,
+                    ].join('&')}`}
+                />
+            )}
+        </>
+    )
 }
